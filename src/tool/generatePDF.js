310d70b92d3024261167aac1da50792d65a0bdd2
cyan92128505 <cyan92128505@gmail.com>
2021-02-02 11:48:34 +0800

    :sparkles: Add generatePDF.js

---
+++ src/tool/generatePDF.js
@@ -0,0 +1,33 @@
+import fs from 'fs-extra';
+import path from 'path';
+import PDFDocument from 'pdfkit';
+
+class GeneratePDF {
+  async Exec() {
+    const storeStream = fs.createWriteStream(path.join(process.cwd(), 'book', 'book.pdf'), { encoding: 'utf-8' });
+    const refStream = fs.createReadStream(path.join(process.cwd(), 'book', 'book.txt'), { encoding: 'utf-8' });
+    const pdfDoc = new PDFDocument();
+    pdfDoc.registerFont(
+      'HiraginoMinchoPro-W3',
+      path.join(process.cwd(), 'assets', 'fonts', 'HiraginoMinchoPro-W3.ttf')
+    );
+    pdfDoc.info['Title'] = 'Book';
+    pdfDoc.info['Author'] = 'Aoma Shinku';
+    pdfDoc.pipe(storeStream).on('finish', async () => {
+      console.log('convert over!');
+      await storeStream.end();
+      await storeStream.close();
+      await refStream.close();
+    });
+
+    refStream.on('error', (err) => console.log);
+    refStream.on('data', async (chunk) => {
+      await pdfDoc.font('HiraginoMinchoPro-W3').text(chunk, { align: 'justify' });
+    });
+    refStream.on('end', async () => {
+      await pdfDoc.end();
+    });
+  }
+}
+
+new GeneratePDF().Exec();
