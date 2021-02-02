1c75938de8e815df13e594e64c23735297ce06d5
cyan92128505 <cyan92128505@gmail.com>
2021-02-02 11:48:17 +0800

    :sparkles: Add translate.js

---
+++ src/tool/translate.js
@@ -0,0 +1,34 @@
+import fs from 'fs-extra';
+import path from 'path';
+import opencc from 'opencc';
+
+class Translate {
+  async Exec() {
+    const converter = new opencc.OpenCC('s2tw.json');
+
+    const sourcePath = path.join(process.cwd(), 'book', 'results');
+    const targetPath = path.join(process.cwd(), 'book', 'zhTW');
+
+    const fileList = await fs.readdir(sourcePath);
+    const fileResult = await Promise.all(
+      fileList.map(async (fileName) => {
+        const filePath = path.join(sourcePath, fileName);
+        const fileContent = await fs.readFile(filePath);
+        console.log(fileName);
+        return await converter.convertPromise(fileContent);
+      })
+    );
+
+    await Promise.all(
+      fileResult.map(async (content, index) => {
+        const filePath = await converter.convertPromise(path.join(targetPath, fileList[index]));
+        await fs.ensureFile(filePath);
+        await fs.writeFile(filePath, content);
+      })
+    );
+
+    console.log('Convert Over!');
+  }
+}
+
+new Translate().Exec();
